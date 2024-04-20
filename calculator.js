a34775d54fc59506f8caea7758dbe9c91f11ac89
Masa Zablah <masazablah2@gmail.com>
2024-04-20 18:18:21 +0300

    update calc function to handle an unknown number of arguments

--- calculator.js
+++ calculator.js
@@ -1,19 +1,36 @@
-function calc (a, operator, b){
-    switch (operator){
-        case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      if (b === 0) {
-        throw new Error('Division by zero');
-      }
-      return a / b;
-    default:
-      throw new Error('Invalid operator');
+// calculator.js
+function calc(...args) {
+    if (args.length < 3 || args.length % 2 === 0) {
+      throw new Error('Invalid input');
     }
-}
-
-module.exports = calc;
+  
+    let result = args[0];
+    for (let i = 1; i < args.length; i += 2) {
+      const operator = args[i];
+      const operand = args[i + 1];
+  
+      switch (operator) {
+        case '+':
+          result += operand;
+          break;
+        case '-':
+          result -= operand;
+          break;
+        case '*':
+          result *= operand;
+          break;
+        case '/':
+          if (operand === 0) {
+            throw new Error('Division by zero');
+          }
+          result /= operand;
+          break;
+        default:
+          throw new Error('Invalid operator');
+      }
+    }
+    return result;
+  }
+  
+  module.exports = calc;
+  
